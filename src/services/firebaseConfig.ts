96f8f7ccb26aa270b5e2ef181d9f51fa2c3bc700
sakugaxl <bekiwe01@gmail.com>
2024-12-08 05:13:30 +0200

    Fix CSP and Firebase login issues

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -14,6 +14,8 @@ const firebaseConfig = {
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID || '',
 };
 
+console.log("Firebase Config:", firebaseConfig);
+
 // Initialize the app
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
